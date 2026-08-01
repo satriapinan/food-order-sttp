bc0486617960a145d225d8e6e2d8252d651a384b
Indra Wahidin Tanjung <indrawahidintanjung22tjg@gmail.com>
2026-08-01 19:34:37 +0700

    edit login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,25 @@
-import { useState } from "react";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import AppButton from "./components/AppButton";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Grid container spacing={2}>
-      {/* MANUAL */}
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          style={{
-            width: "100%",
-            backgroundColor: "#1976d2",
-            borderRadius: "5px",
-            border: "none",
-            color: "#FFF",
-            padding: "10px",
-            cursor: "pointer",
-          }}
-        >
-          TESTING
-        </button>
-      </Grid>
+    <div className="container">
+      <div className="login-card">
+        <h1>Food-Order</h1>
+        <p>Login ke dalam sistem</p>
 
-      {/* MENGGUNAKAN MUI */}
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setCount((count) => count + 1)}
-          sx={{
-            width: "100%",
-            borderRadius: "5px",
-            boxShadow: "none",
-          }}
-        >
-          MUI
-        </Button>
-      </Grid>
+        <div className="input-group">
+          <label>Username</label>
+          <input type="text" placeholder="Masukkan username" />
+        </div>
 
-      {/* CUSTOM COMPONENT */}
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <AppButton onClick={() => setCount((count) => count + 1)}>
-          Tambah 1 angka
-        </AppButton>
-      </Grid>
+        <div className="input-group">
+          <label>Password</label>
+          <input type="password" placeholder="Masukkan password" />
+        </div>
 
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <AppButton onClick={() => setCount((count) => count + 2)}>
-          Tambah 2 angka
-        </AppButton>
-      </Grid>
-
-      {/* HASIL */}
-      <Grid size={{ xs: 12 }}>
-        <h2>{count}</h2>
-      </Grid>
-    </Grid>
+        <button>LOGIN</button>
+      </div>
+    </div>
   );
 }
 
