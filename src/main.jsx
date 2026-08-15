9590d39ab8447d05cceb101ef8bea87ff0f8d06c
Indra Wahidin Tanjung <indrawahidintanjung22tjg@gmail.com>
2026-08-15 10:28:00 +0700

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import AuthProviders from "./context/AuthProviders.jsx";
+import AuthProviders from "./providers/AuthProvider.jsx";
 
 import "./index.css";
 
